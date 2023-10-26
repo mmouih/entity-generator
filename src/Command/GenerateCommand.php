63b62b0b9fc090509d716df05d9567ce432d8ac6
Mounir Mouih <mounir.mouih@gmail.com>
2023-10-26 20:56:54 +0200

    improve console output information

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace EntityGenerator\Command;
 
+use EntityGenerator\Type\GenerateCommandArgs;
+use EntityGenerator\Handler\GenerationProcess;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use EntityGenerator\Handler\GenerationProcess;
-use EntityGenerator\Type\GenerateCommandArgs;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -27,6 +28,8 @@ class GenerateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('start generating');
+        $io = new SymfonyStyle($input, $output);
+
         try {
             $printed = $this->classGenerationHandler->handle(GenerateCommandArgs::fromData([
                 'className' => $input->getArgument('className'),
@@ -34,13 +37,13 @@ class GenerateCommand extends Command
                 'format' => $input->getArgument('format'),
                 'source' => $input->getOption('source'),
             ]));
-            $output->writeln('files generated with success in:');
-            $output->writeln($printed);
+            $io->success('Entities generated with success in:');
+            $io->info($printed);
         } catch (\Throwable $excepetion) {
-            $output->writeln([
-                'message' => 'An error has occured while generating files : ' . $excepetion->getMessage(),
-                'file' => $excepetion->getFile(),
-                'line' => $excepetion->getLine(),
+            $io->getErrorStyle()->error('An error has occured while generating entities:');
+            $io->getErrorStyle()->info([
+                'message' => $excepetion->getMessage(),
+                'file' => $excepetion->getFile() . 'on line ' . $excepetion->getLine()
             ]);
 
             return Command::FAILURE;
