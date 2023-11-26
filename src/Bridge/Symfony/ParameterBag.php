a8a60e524d1005773ace9aa18e20342d87f436a0
Mounir Mouih <mounir.mouih@gmail.com>
2023-11-26 22:15:05 +0100

    remove unused classes

--- src/Bridge/Symfony/ParameterBag.php
+++ src/Bridge/Symfony/ParameterBag.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace EntityGenerator\Bridge\Symfony;
 
-use UnitEnum;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface as SymfonyParamBag;
 
 /**
